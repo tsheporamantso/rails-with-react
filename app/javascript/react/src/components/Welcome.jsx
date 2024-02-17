42f5ed9d291d4c82ffa15710eb922b22cad92c0b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-17 17:06:17 +0200

    Setup react directorates and Welcome component

---
+++ app/javascript/react/src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const Welcome = () => {
+  return(
+    <div className="containe">
+      <h1>Welcome to Rails with React Question List</h1>
+      <p className="lead"><em>by Tshepo Gladwin Ramantso</em></p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('welcome'))
+root.render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>
+)
+
+export default Welcome 
