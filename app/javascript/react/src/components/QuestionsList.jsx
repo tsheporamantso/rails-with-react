92c57a7d84ecafcca96d5c84ba6f68befe819a75
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-18 18:36:03 +0200

    Add key for each child element

--- app/javascript/react/src/components/QuestionsList.jsx
+++ app/javascript/react/src/components/QuestionsList.jsx
@@ -43,7 +43,7 @@ const QuestionsList = () => {
     <div className="row">
       <div className="col-lg-10 mx-auto">
         {questionsList.map((question) =>
-          <div className="card rounded-3 mt-3">
+          <div className="card rounded-3 mt-3" key={question.id}>
             <div className="card-body">
               <h3 className="card-title">{question.title}</h3>
               <p className="lead">
@@ -52,7 +52,6 @@ const QuestionsList = () => {
             </div>
           </div>
         )}
-    
       </div>
     </div>
   )
